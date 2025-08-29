853f45d73742f628b26fbe08cff215374c057e73
/GaiTod✅ <phichitphu2015@gmail.com>
2025-08-29 21:16:29 +0700

    fix_ui นิดหน่อย
    แก้  ui นิดเดียว เดียวมาทำต่อ

--- GG_note.py
+++ GG_note.py
@@ -1,4 +1,6 @@
 import tkinter as tk
+from tkinter import filedialog, Text
+import os
 
 def add_note():
     note = entry.get()
@@ -9,13 +11,16 @@ def add_note():
 root = tk.Tk()
 root.title("GG_note")
 
+canvas =tk.Canvas(root, height=700, width=700, bg="#ffffff")
+canvas.pack(side=tk.RIGHT)
+
 entry = tk.Entry(root, width=40)
 entry.pack()
 
 btn = tk.Button(root, text="Capture Your Spark", command=add_note)
 btn.pack()
 
-listbox = tk.Listbox(root, width=50, height=10)
-listbox.pack()
+listbox = tk.Listbox(root, width=50, height=20)
+listbox.pack(side=tk.RIGHT)
 
 root.mainloop()
