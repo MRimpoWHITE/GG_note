0c579d5dee2d10b9926689d15a0cd87b9316dfbe
/GaiTod✅ <phichitphu2015@gmail.com>
2025-09-02 02:11:23 +0700

    Fix_placeholder
    ปรับแก้ placeholder
    ทำให้ฟังก์ชันโน๊ตเรียบร้อย

--- GG_note.py
+++ GG_note.py
@@ -2,6 +2,7 @@ import customtkinter as ctk
 from customtkinter import *
 from CTkListbox import *
 
+
 ctk.set_appearance_mode("dark")      #เลือก ธีม
 ctk.set_default_color_theme("blue")    
 
@@ -9,55 +10,82 @@ app = ctk.CTk()
 app.title("GG_List Note")
 app.geometry("500x400")
 
-tasks = []
+note_names = []
+notes = []
 
-# ----------------------
+# --------------------
+# Save Note as File
+# --------------------
+
+
+
+
+
+
+
+
+
+# -------------
 # function
-# ----------------------
+# -------------
 
-def add_task():
-    task = entry.get()
-    if task != "":
-        tasks.append(task)
-        listbox.insert(END, task)
+placeholder = "Capture Your Spark...."
+
+def add_note():
+    note_name = entry.get()
+    if note_name != "":
+        note_names.append(note_name)
+        notes.append("")
+        listbox.insert(END, note_name)
         entry.delete(0, END)
 
 def delete_task():
     selected = listbox.curselection()
-    if selected is not None and selected != "":
+    if selected is not None and selected !="":
         index =  int(selected)
         listbox.delete(index)
-        tasks.pop(index)  # ต้อง cast เป็น int ด้วย
+        note_names.pop(index)  # ต้อง cast เป็น int ด้วย
         note_text.delete("1.0", "end")   # ล้างช่อง note ด้วย
         
 def show_task(event):
     selected = listbox.curselection()
-    if selected is not None and selected != "":
+    if selected is not None and selected !="":
         index = int(selected)
         note_text.delete("1.0", "end")
-        note_text.insert("2.0", tasks[index])
+        note_text.insert("1.0", notes[index])
+        
+        if notes[index].strip() == "":
+            note_text.insert("1.0", placeholder, "placeholder")
+            note_text.tag_add("placeholder", "1.0", "end")
+        # else:
+        #     note_text.insert("1.0", notes[index])
+        
+        print(note_names[index])
+        print(notes[index])
 
 def save_task():
     selected = listbox.curselection()
-    if selected is not None and selected != "":
+    if selected is not None and selected !="":
         index = int(selected)
         new_text = note_text.get("1.0", "end-1c")
-        tasks[index] = new_text
+        notes[index] = new_text
+        print("note_saved")
+    
         
 # ----------------------
 # Placeholder functions
 # ----------------------
         
 def clear_placeholder(event):                             
-    if note_text.get("1.0", "end-1c") == "Capture Your Spark....":
+    if note_text.get("1.0", "end-1c") == placeholder:
         note_text.delete("1.0", "end")
         note_text.tag_remove("placeholder", "1.0", "end")
         
 def add_placeholder(event):
     if note_text.get("1.0", "end-1c").strip()== "":
-        note_text.insert("1.0", "Capture Your Spark....", "placeholder")
+        note_text.insert("1.0", placeholder, "placeholder")
         note_text.tag_add("placeholder", "1.0", "end")
-        
+
 
 # ----------------------
 # Widgets
@@ -72,7 +100,7 @@ frame2.grid(row=0, column=1, pady=20, padx=20, sticky="nsew")
 entry = ctk.CTkEntry(frame1, placeholder_text="Name Your Journey")
 entry.pack(pady=10, fill="x")
 
-add_button = ctk.CTkButton(frame1, text="Add", command=add_task)
+add_button = ctk.CTkButton(frame1, text="Add", command=add_note)
 add_button.pack(pady=5)
 
 del_button = ctk.CTkButton(frame1, text="Delete", command=delete_task)
@@ -85,23 +113,22 @@ listbox = CTkListbox(frame1, height=200)
 listbox.pack(pady=10, fill="both", expand=True)
 listbox.bind("<<ListboxSelect>>", show_task)
 
-# ----------------------
+# ---------
 # Note
-# ----------------------  
+# ---------
 
 note_text = ctk.CTkTextbox(frame2)
 note_text.pack(pady=10, fill="both", expand=True)
 # ตั้งค่า tag สำหรับ placeholder
 note_text.tag_config("placeholder", foreground="gray")   #เปลี่ยนสี
-note_text.insert("1.0", "Capture Your Spark....", "placeholder")
+note_text.insert("1.0", placeholder, "placeholder")
 note_text.tag_add("placeholder", "1.0", "end")
 # bind event focus
 note_text.bind("<FocusIn>", clear_placeholder)          
 note_text.bind("<FocusOut>", add_placeholder)          
 
-
-# ----------------------
+# -------------
 # Run app
-# ----------------------
+# -------------
 
 app.mainloop()
